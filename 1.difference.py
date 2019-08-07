a58e2cf23a4db6fa3e6ba3bf3f8d55701a4e0e1b
miltonmarsalino <46523397+miltonmarsalino@users.noreply.github.com>
2019-08-07 00:21:54 -0900

    Create 1.difference.py

---
+++ 1.difference.py
@@ -0,0 +1,9 @@
+# your code goes here
+def difference(n):
+	if n<=17:
+		return 17-n
+	else:
+		return(n-17)*2
+print(difference(22))
+print(difference(14))ijayaramanp/2021-batch-training/blob/master/Python_program_questions.rtf
+		

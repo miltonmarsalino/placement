ea2a0ad5b6c673de97903a8ca92eee25bdc4795b
miltonmarsalino <46523397+miltonmarsalino@users.noreply.github.com>
2019-08-07 00:43:04 -0900

    Create 1.4concatenate.py

---
+++ 1.4concatenate.py
@@ -0,0 +1,7 @@
+# your code goes here
+def concatenate_list_datalist:
+	result="1","10","5', "4"9
+	for element in list:
+		result += str(elements)
+	return result	
+print(concatinate_list_data([1,5,12,2]))
